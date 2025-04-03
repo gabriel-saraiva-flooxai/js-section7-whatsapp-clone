2d736b03639491319ad7fc9a9ddd2f4e21dc325d
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-03 15:56:12 -0300

    feat: add photo capture and re-take functionality; implement file preview before sending

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,5 +1,6 @@
 import {Format} from '../util/Format';
 import {CameraController} from './CameraController';
+import {DocumentPreviewController} from './DocumentPreviewController';
 
 export class WhatsAppController {
     
@@ -220,13 +221,38 @@ export class WhatsAppController {
 
             this.closeAllMainPanel();
             this.el.panelMessagesContainer.show();
+            this._camera.stop();
 
         });
 
         this.el.btnTakePicture.on('click', e => {
 
-            console.log('take picture');
+            let dataUrl = this._camera.takePicture();
             
+            this.el.pictureCamera.src = dataUrl;
+            this.el.pictureCamera.show();
+            this.el.videoCamera.hide();
+            this.el.btnReshootPanelCamera.show();
+            this.el.containerTakePicture.hide();
+            this.el.containerSendPicture.show();
+            
+        });
+
+        this.el.btnReshootPanelCamera.on('click', e => {
+
+            this.el.pictureCamera.hide();
+            this.el.videoCamera.show();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.containerTakePicture.show();
+            this.el.containerSendPicture.hide();
+
+        });
+
+        this.el.btnSendPicture.on('click', e => {
+
+            console.log(this.el.pictureCamera.src);
+            
+
         });
 
         this.el.btnAttachDocument.on('click', e => {
@@ -236,7 +262,57 @@ export class WhatsAppController {
             this.el.panelDocumentPreview.css({
                 'height':'calc(100% - 120px)'
             });
+            this.el.inputDocument.click();
         
+        });
+
+        this.el.inputDocument.on('change', e => {
+
+            if (this.el.inputDocument.files.length) {
+
+            let file = this.el.inputDocument.files[0];
+
+            this._documentPreviewController = new DocumentPreviewController(file);
+
+            this._documentPreviewController.getPreviewData().then(result => {
+
+                this.el.imgPanelDocumentPreview.src = result.src;
+                this.el.infoPanelDocumentPreview.innerHTML = result.info;
+                this.el.imagePanelDocumentPreview.show();
+                this.el.filePanelDocumentPreview.hide();
+
+            }).catch(err => {
+
+                switch (file.type) {
+
+                    case 'application/vnd.ms-excel':
+                    case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+                        this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-xls';
+                    break;
+                    
+                    case 'application/vnd.ms-powerpoint':
+                    case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+                        this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-ppt';
+                    break;
+
+                    case 'application/msword':
+                    case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                        this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-doc';
+                    break;
+                        
+                    default:
+                        this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-generic';
+                    break;
+                }
+                
+                this.el.filenamePanelDocumentPreview.innerHTML = file.name;
+                this.el.imagePanelDocumentPreview.hide();
+                this.el.filePanelDocumentPreview.show();
+
+            });
+            
+            }
+
         });
 
         this.el.btnClosePanelDocumentPreview.on('click', e => {
