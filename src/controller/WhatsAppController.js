e661a1339f048bfe549978d1d0a19e6a2ec188a6
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-04 16:41:43 -0300

    feat: update user profile data

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -24,15 +24,34 @@ export class WhatsAppController {
         this._firebase.initAuth()
         .then(response => {
             
-            this._user = new User();
+            this._user = new User(response.user.email);
 
-            let userRef = User.findByEmail(response.user.email);
+            this._user.on('datachange', data => {
 
-            userRef.set({
-                name: response.user.displayName,
-                email: response.user.email,
-                photo: response.user.photoURL
-            }).then(() => {
+                document.querySelector('title').innerHTML = data.name + ' - WhatsApp Clone';
+
+                this.el.inputNamePanelEditProfile.innerHTML = data.name;
+
+                if (data.photo){
+
+                    let photo = this.el.imgPanelEditProfile;
+                    photo.src = data.photo;
+                    photo.show();
+                    this.el.imgDefaultPanelEditProfile.hide();
+
+                    let photo2 = this.el.myPhoto.querySelector('img');
+                    photo2.src = data.photo;
+                    photo2.show();
+
+                }
+
+            });
+
+            this._user.name = response.user.displayName;
+            this._user.name = response.user.email;
+            this._user.name = response.user.photoURL;
+
+            this._user.save().then(() => {
 
                 this.el.appContent.css({
                     display:'flex'
@@ -184,8 +203,16 @@ export class WhatsAppController {
 
         this.el.btnSavePanelEditProfile.on('click', e => {
 
-            console.log(this.el.inputNamePanelEditProfile.innerHTML);
-            
+            this.el.btnSavePanelEditProfile.disabled = true;
+
+            this._user.name = this.el.inputNamePanelEditProfile.innerHTML;
+
+            this._user.save().then(() => {
+
+                this.el.btnSavePanelEditProfile.disabled = false;
+
+            });
+
         });
 
         this.el.formPanelAddContact.on('submit', e => {
