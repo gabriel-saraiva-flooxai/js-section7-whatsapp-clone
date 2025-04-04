2e33367aaf9b8d3a5a9bc4691d8c90cbb24f82ce
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-04 12:52:27 -0300

    chore: setup Firebase

---
+++ src/util/Firebase.js
@@ -0,0 +1,48 @@
+const firebase = require('firebase');
+require('firebase/firestore');
+
+export class Firebase {
+
+    constructor(){
+
+        this._config  = {
+            apiKey: "AIzaSyDEfBD6SohzT_E2IgVVSa6zqYiQVEsB-i0",
+            authDomain: "whatsapp-clone-saas.firebaseapp.com",
+            projectId: "whatsapp-clone-saas",
+            storageBucket: "whatsapp-clone-saas.firebasestorage.app",
+            messagingSenderId: "561114180097",
+            appId: "1:561114180097:web:7abfbde2cecea087f4ae00",
+            measurementId: "G-2SHPW96DMV"
+        };
+
+        this.init();
+
+    }
+
+    init(){
+
+        if (!this._initialized) {        
+            firebase.initializeApp(this._config);
+
+            firebase.firestore().settings({
+                timestampsInSnapshots: true
+            });
+
+            this._initialized = true;
+        }
+
+    }
+
+    static db(){
+
+        return firebase.firestore();
+
+    }
+
+    static hd(){
+
+        return firebase.storage();
+
+    }
+
+}
