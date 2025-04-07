53f693c4cdfa90612681b32b8639788dcf03cad6
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-07 15:20:51 -0300

    feat: send and receive messages in real time

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -5,6 +5,7 @@ import { DocumentPreviewController } from './DocumentPreviewController';
 import { Firebase } from "../util/Firebase";
 import { User } from "../model/User";
 import { Chat } from '../model/Chat';
+import { Message } from '../model/Message';
 
 export class WhatsAppController {
     
@@ -140,21 +141,7 @@ export class WhatsAppController {
 
                 div.on('click', e => {
 
-                    console.log('chatId', contact.chatId);
-                    
-                    this.el.activeName.innerHTML = contact.name;
-                    this.el.activeStatus.innerHTML = contact.status;
-                    
-                    if (contact.photo) {
-                        let img = this.el.activePhoto;
-                        img.src = contact.photo;
-                        img.show();
-                    }              
-                    
-                    this.el.home.hide();
-                    this.el.main.css({
-                        display:'flex'
-                    });
+                    this.setActiveChat(contact);
 
                 });
             
@@ -166,6 +153,58 @@ export class WhatsAppController {
 
         this._user.getContacts()
 
+    }
+
+    setActiveChat(contact){
+
+        if (this._contactActive) {
+            Message.getRef(this._contactActive.chatId).onSnapshot(()=>{});
+        }
+
+        this._contactActive = contact;
+
+        this.el.activeName.innerHTML = contact.name;
+        this.el.activeStatus.innerHTML = contact.status;
+        
+        if (contact.photo) {
+            let img = this.el.activePhoto;
+            img.src = contact.photo;
+            img.show();
+        }              
+        
+        this.el.home.hide();
+        this.el.main.css({
+            display:'flex'
+        });
+
+        Message.getRef(this._contactActive.chatId).orderBy('timeStamp').onSnapshot(docs => {
+
+            this.el.panelMessagesContainer.innerHTML = '';
+
+            docs.forEach(doc => {
+
+                let data = doc.data();
+                data.id = doc.id;
+                
+                if (!this.el.panelMessagesContainer.querySelector('#' + data.id))
+                {
+
+                    let message = new Message();
+
+                    message.fromJSON(data);
+
+                    let me = (data.from === this._user.email);
+
+                    let view = message.getViewElement(me);
+
+                    this.el.panelMessagesContainer.appendChild(view);
+
+                }
+
+            });
+
+        });
+    
     }
 
     loadElements(){
@@ -619,9 +658,16 @@ export class WhatsAppController {
 
         this.el.btnSend.on('click', e => {
 
-            console.log(this.el.inputText.innerHTML);
-            
+            Message.send(
+                this._contactActive.chatId, 
+                this._user.email,
+                'text',
+                this.el.inputText.innerHTML
+            );
 
+            this.el.inputText.innerHTML = '';
+            this.el.panelEmojis.removeClass('open');
+            
         });
 
         this.el.btnEmojis.on('click', e => {
